53374488d4f989395261ecaac7d658cfd2c3f070
Kalloreinder <matthieu.perrelle@gmail.com>
2025-08-30 14:56:40 +0200

    using global var for book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters
+BOOK_PATH = "books/frankenstein.txt"
 
 def get_book_text(filepath):
 
@@ -9,7 +10,7 @@ def get_book_text(filepath):
 
 def main():
 
-    current_book = get_book_text("books/frankenstein.txt")
+    current_book = get_book_text(BOOK_PATH)
     num_words = count_words(current_book)
     num_letters = count_characters(current_book)
     print(f"{len(num_words)} words found in the document")
