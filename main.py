fdfff58af5da2ad0f70a097b73c76b0eb18e3941
Kalloreinder <matthieu.perrelle@gmail.com>
2025-08-30 15:06:52 +0200

    some cleaning

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from stats import count_words, count_characters
 BOOK_PATH = "books/frankenstein.txt"
 
+
 def get_book_text(filepath):
 
     with open(filepath, 'r', encoding='utf-8') as f:
         file_contents = f.read()
-    
+
     return file_contents
+
 
 def main():
 
@@ -15,6 +17,7 @@ def main():
     num_letters = count_characters(current_book)
     print(f"{len(num_words)} words found in the document")
     print(num_letters)
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ def count_words(document):
 def count_characters(document):
     doc = document.lower()
     letters = {}
-    
+
     for letter in doc:
         if letter in letters:
             letters[letter] += 1
